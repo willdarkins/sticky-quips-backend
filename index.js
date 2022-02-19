06f661f13311d8e96cb77a0a119e2f291371d785
Will Darkins <willdarkins@gmail.com>
2022-02-18 23:10:33 -0800

    played around with api calls for requests and install nodemon

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+app.get('/', (req, res) => {
+    res.send("hello world!!!")
+})
+
+app.get('/info', (req, res) => {
+    const notes = [
+        {
+            text: "Fuck a goat",
+            link: "https://petco.com"
+        },
+        {
+            text: "jerk off",
+            link: "https://pornhub.com"
+        },
+
+    ]
+    res.json({notes})
+})
+
 app.listen(port, () => {
-    console.log(`🌎 sticky-quips-backend running on port https://localhost:${port}`)
+    console.log(`🌎 sticky-quips-backend running on port http://localhost:${port}`)
 })
