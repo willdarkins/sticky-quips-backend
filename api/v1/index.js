fa2204f6ab4bd5e82500d2d71bd77d82394b915f
Will Darkins <willdarkins@gmail.com>
2022-02-21 16:00:09 -0800

    added various routes for post, get and delete

--- api/v1/index.js
+++ api/v1/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Note = require('../../db/models/note.model')
 var notesRouter = express.Router();
 
+//get full note list
 notesRouter.get('/', (req, res) => {
     Note.find({}, (err, notes) => {
         if (err) {
@@ -12,6 +13,27 @@ notesRouter.get('/', (req, res) => {
         res.json({
             notes
         })
+    })
+})
+
+//get individual note by ID
+notesRouter.get('/:id', (req, res) => {
+    res.json({
+        reply: 'note by id success'
+    })
+})
+
+//post a note
+notesRouter.post('/', (req, res) => {
+    res.json({
+        reply: 'note created'
+    })
+})
+
+//delete note by ID
+notesRouter.delete('/:id', (req, res) => {
+    res.json({
+        reply: 'note has been deleted'
     })
 })
 
